0f5412fedad3e93fe9ba672da0906c8c2174459b
Gabriel Henrique Duarte Barbosa <ghenriqu@c1r12s1.42porto.com>
2025-11-01 17:45:48 +0000

    update

--- vbc/2/2.c
+++ vbc/2/2.c
@@ -1,46 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   1.c                                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ghenriqu <ghenriqu@student.42porto.com>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/25 16:34:33 by ghenriqu          #+#    #+#             */
-/*   Updated: 2025/11/01 13:23:56 by ghenriqu         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-// Assignment name:		vbc
-// Expected files:		*.c *h
-// Allowed functions:	malloc, calloc, realloc, free, printf, isdigit, write
-
-// Write a program that prints the result of a mathematical expression given as argument.
-// It must handle addition, multiplication and parenthesis. All values are between 0 and 9 included.
-// In case of an unexpected symbol, you must print "Unexpected token '%c'\n".
-// If the expression ends unexpectedly, you must print "Unexpected end of input\n".
-// The same rule applies if finding an unexpected '(' or ')'.
-// In case of a syscall failure, you must exit with 1.
-
-// Examples:
-// $> ./vbc '1' | cat -e
-// 1$
-// $> ./vbc '2+3' | cat -e
-// 5$
-// $> ./vbc '3*4+5' | cat -e
-// 17$
-// $> ./vbc '3+4*5' | cat -e
-// 23$
-// $> ./vbc '(3+4)*5' | cat -e
-// 35$
-// $> ./vbc '(((((2+2)*2+2)*2+2)*2+2)*2+2)*2' | cat -e
-// 188$
-// $> ./vbc '1+'
-// Unexpected end of input
-// $> ./vbc '1+2)' | cat -e
-// Unexpected token ')'$
-
-// file at the exam:
-
 #include <stdio.h>
 #include <malloc.h>
 #include <ctype.h>
@@ -51,9 +8,9 @@ typedef struct node {
         MULTI,
         VAL
     }   type;
-    int 		val;
-    struct node	*l;
-    struct node	*r;
+    int val;
+    struct node *l;
+    struct node *r;
 }   node;
 
 node    *new_node(node n)
@@ -103,7 +60,6 @@ int expect(char **s, char c)
     return (0);
 }
 
-//...
 static node	*parse_factor(char **s);
 static node	*parse_term(char **s);
 static node	*parse_expr_r(char **s);
@@ -165,7 +121,7 @@ static node	*parse_expr_r(char **s)
 		node	*right = parse_term(s);
 		if (!right)
 		{
-			destroy_tree (left);
+			destroy_tree(left);
 			return (NULL);
 		}
 		node	n = { .type = ADD, .l = left, .r = right };
@@ -178,9 +134,9 @@ static node	*parse_expr_r(char **s)
 
 node    *parse_expr(char *s)
 {
-	char	*p = s;
+    char	*p = s;
 	node	*ret = parse_expr_r(&p);
-	
+
 	if (!ret)
 		return (NULL);
     if (*p)
@@ -191,8 +147,6 @@ node    *parse_expr(char *s)
     }
     return (ret);
 }
-
-// ...
 
 int eval_tree(node *tree)
 {
